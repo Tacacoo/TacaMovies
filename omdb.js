5e40923744a9726258df2ee7f9a0035eaa41fcb8
Brayan Melendez <xshikkii@gmail.com>
2019-06-18 10:08:13 -0600

    img no disponible

--- omdb.js
+++ omdb.js
@@ -25,13 +25,26 @@
             movieList.empty();
             for (var n in response.Search) {
                 var movie = response.Search[n];
-                var a = $('<a href="#"></a>');
-                var li = $('<li class="list-group-item"></li>');
-                var img = $('<img src="' + movie.Poster + '" width="50px"></img>');
-                a.append(img);
-                a.append(movie.Title);
-                li.append(a);
-                movieList.append(li);
+                if(movie.Poster!=="N/A"){             
+                    var a = $('<a href="#"></a>');
+                    var li = $('<li class="list-group-item"></li>');
+                    var img = $('<img src="' + movie.Poster + '" width="50px"></img>');
+                    a.append(img);
+                    a.append(movie.Title);
+                    li.append(a);
+                    movieList.append(li);
+                }
+                else{
+
+                    var a = $('<a href="#"></a>');
+                    var li = $('<li class="list-group-item"></li>');
+                    var img = $('<img src="https://www.cocin-cartagena.es/wp-content/uploads/2018/12/ImagenNoDisponible.png" width="50px"></img>');
+                    a.append(img);
+                    a.append(movie.Title);
+                    li.append(a);
+                    movieList.append(li);
+                }
+                
             }
 
         }
@@ -59,19 +72,36 @@
         function cardMoviesNew(response) {        
             for (var m in response.Search) { //recorrer el objeto retornado
                 var movie = response.Search[m];
-                var img = $('<img src="' + movie.Poster + '" class="image-grid">');
+                if(movie.Poster!=="N/A"){
+                    var img = $('<img src="' + movie.Poster + '" class="image-grid">');
                 var a = $('<a href="#"></a>');
                 a.append(img);
                 cards.append(a);
+                }
+                else{               
+                var img = $('<img src="https://www.cocin-cartagena.es/wp-content/uploads/2018/12/ImagenNoDisponible.png" class="image-grid">');
+                var a = $('<a href="#"></a>');
+                a.append(img);
+                cards.append(a);
+                }
             }
         }
+        
         function cardMoviesSeries(response) {           
             for (var m in response.Search) { //recorrer el objeto retornado
                 var movie = response.Search[m];
+                if(movie.Poster!=="N/A"){
                 var img = $('<img src="' + movie.Poster + '" class="image-grid">');
                 var a = $('<a href="#"></a>');
                 a.append(img);
                 cards2.append(a);
+                }
+                else{
+                var img = $('<img src="https://www.cocin-cartagena.es/wp-content/uploads/2018/12/ImagenNoDisponible.png" class="image-grid">');
+                var a = $('<a href="#"></a>');
+                a.append(img);
+                cards2.append(a);
+                }
             }
         }
 
